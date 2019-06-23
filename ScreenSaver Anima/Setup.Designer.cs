c091338754404c58905c191ea34e1075e96da520
Minty Roma <srdhe4@yandex.ru>
2019-06-24 01:37:36 +0300

    Added registry data load

--- ScreenSaver Anima/Setup.Designer.cs
+++ ScreenSaver Anima/Setup.Designer.cs
@@ -30,76 +30,135 @@
         {
             this.VolumeBar = new System.Windows.Forms.TrackBar();
             this.EnableVolumeControlCB = new System.Windows.Forms.CheckBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.VideoList = new System.Windows.Forms.ListBox();
+            this.BrowseVideo = new System.Windows.Forms.Button();
+            this.DeleteVideo = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.VolumeBar)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // VolumeBar
             // 
-            this.VolumeBar.Location = new System.Drawing.Point(289, 21);
+            this.VolumeBar.Location = new System.Drawing.Point(6, 37);
             this.VolumeBar.Maximum = 100;
             this.VolumeBar.Name = "VolumeBar";
-            this.VolumeBar.Size = new System.Drawing.Size(227, 45);
+            this.VolumeBar.Size = new System.Drawing.Size(271, 45);
             this.VolumeBar.TabIndex = 0;
             this.VolumeBar.TickFrequency = 10;
             // 
             // EnableVolumeControlCB
             // 
             this.EnableVolumeControlCB.AutoSize = true;
-            this.EnableVolumeControlCB.Location = new System.Drawing.Point(289, 63);
+            this.EnableVolumeControlCB.Location = new System.Drawing.Point(6, 88);
             this.EnableVolumeControlCB.Name = "EnableVolumeControlCB";
-            this.EnableVolumeControlCB.Size = new System.Drawing.Size(227, 56);
+            this.EnableVolumeControlCB.Size = new System.Drawing.Size(227, 43);
             this.EnableVolumeControlCB.TabIndex = 1;
-            this.EnableVolumeControlCB.Text = "Управление клавишами\r\nРазрешить управление громкостью\r\nво время воспроизведения с" +
-    " помощью\r\nклавиш <Вверх> и <Вниз>";
+            this.EnableVolumeControlCB.Text = "Разрешить управление громкостью\r\nво время воспроизведения с помощью\r\nклавиш <Ввер" +
+    "х> и <Вниз>";
             this.EnableVolumeControlCB.UseVisualStyleBackColor = true;
             // 
-            // listBox1
+            // VideoList
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 63);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(271, 95);
-            this.listBox1.TabIndex = 2;
+            this.VideoList.FormattingEnabled = true;
+            this.VideoList.Location = new System.Drawing.Point(6, 15);
+            this.VideoList.Name = "VideoList";
+            this.VideoList.Size = new System.Drawing.Size(271, 95);
+            this.VideoList.TabIndex = 2;
+            // 
+            // BrowseVideo
+            // 
+            this.BrowseVideo.Location = new System.Drawing.Point(6, 116);
+            this.BrowseVideo.Name = "BrowseVideo";
+            this.BrowseVideo.Size = new System.Drawing.Size(142, 23);
+            this.BrowseVideo.TabIndex = 3;
+            this.BrowseVideo.Text = "Добавить видео";
+            this.BrowseVideo.UseVisualStyleBackColor = true;
+            // 
+            // DeleteVideo
+            // 
+            this.DeleteVideo.Location = new System.Drawing.Point(198, 117);
+            this.DeleteVideo.Name = "DeleteVideo";
+            this.DeleteVideo.Size = new System.Drawing.Size(74, 23);
+            this.DeleteVideo.TabIndex = 4;
+            this.DeleteVideo.Text = "Удалить";
+            this.DeleteVideo.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.VolumeBar);
+            this.groupBox1.Controls.Add(this.EnableVolumeControlCB);
+            this.groupBox1.Location = new System.Drawing.Point(12, 180);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 164);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Громкость";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.VideoList);
+            this.groupBox2.Controls.Add(this.BrowseVideo);
+            this.groupBox2.Controls.Add(this.DeleteVideo);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(287, 162);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Воспроизводимые видео";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 21);
+            this.button1.Location = new System.Drawing.Point(224, 369);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(271, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Добавить видео";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Отмена";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(221, 164);
+            this.button2.Location = new System.Drawing.Point(136, 369);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(62, 33);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Удалить";
+            this.button2.Size = new System.Drawing.Size(82, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Принять";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(48, 369);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Сохранить";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Setup
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(625, 324);
+            this.ClientSize = new System.Drawing.Size(311, 404);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.EnableVolumeControlCB);
-            this.Controls.Add(this.VolumeBar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Setup";
             this.Text = "Setup";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Setup_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.VolumeBar)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -107,8 +166,13 @@
 
         private System.Windows.Forms.TrackBar VolumeBar;
         private System.Windows.Forms.CheckBox EnableVolumeControlCB;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox VideoList;
+        private System.Windows.Forms.Button BrowseVideo;
+        private System.Windows.Forms.Button DeleteVideo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- ScreenSaver Anima/Setup.cs
+++ ScreenSaver Anima/Setup.cs
@@ -12,14 +12,20 @@ namespace ScreenSaver_Anima
 {
     public partial class Setup : Form
     {
-        private string status = "";
-        private bool edited = false;
 
-        List<string> videos = new List<string>();
+        private List<string> videos = new List<string>();
+        private int volume = 50;
+        private bool allowkeys = false;
         public Setup()
         {
             InitializeComponent();
-            InitVideos();
+            CheckReg();
+            videos = InitVideos();
+            volume = InitVolume();
+            allowkeys = InitAllow();
+            AddVideos();
+            EnableVolumeControlCB.Checked = allowkeys;
+            VolumeBar.Value = volume;
         }
 
         private void Setup_FormClosing(object sender, FormClosingEventArgs e)
@@ -28,43 +34,115 @@ namespace ScreenSaver_Anima
             Application.Exit();
         }
 
-        public string Status
-        {
-            get { return status; }
-            set
-            {
-                edited = true;
-                status = value;
-            }
-        }
-
+        /// <summary>
+        /// Считывает из реестра список воспроизводимых видео
+        /// </summary>
+        /// <returns>Список путей с видео</returns>
         private List<string> InitVideos()
         {
-            List<string> videos = new List<string>();
+            List<string> video = new List<string>();
             RegistryKey WorkKey = Registry.CurrentUser;
-            try
+            WorkKey = WorkKey.OpenSubKey(@"Software\AnimaScreenSaver");
+            string alldata = Convert.ToString(WorkKey.GetValue("Play"));
+            string[] vid = alldata.Split('|');
+            for (int i = 0; i<vid.Length;i++)
             {
-                WorkKey = WorkKey.OpenSubKey(@"Software\AnimaScreenSaver");
-                string alldata = Convert.ToString(WorkKey.GetValue("Play"));
-                string[] vid = alldata.Split('|');
-                for (int i = 0; i<vid.Length;i++)
-                {
-                    videos.Add(vid[i]);
+                video.Add(vid[i]);
 #if DEBUG
-                    MessageBox.Show(vid[i]);
+                MessageBox.Show(vid[i]);
 #endif
-                }
-
-
             }
-            catch
+            return video;
+        }
+
+        /// <summary>
+        /// Считывает из реестра значение звука от 0 до 100.
+        /// В случае если значение в реестре больше чем 100 или меньше 0,
+        /// то автоматически исправляет на 50
+        /// </summary>
+        /// <returns>Возвращает громость от 0 до 100</returns>
+        private int InitVolume()
+        {
+            int vol = 50;
+            RegistryKey WorkKey = Registry.CurrentUser.OpenSubKey(@"Software\AnimaScreenSaver");
+            vol = Convert.ToInt32(WorkKey.GetValue("Volume"));
+            if (vol > 100 || vol < 0)
             {
-                WorkKey = WorkKey.OpenSubKey(@"Software");
-                WorkKey.CreateSubKey("AnimaScreenSaver");
-                WorkKey = WorkKey.OpenSubKey("AnimaScreenSaver");
-                WorkKey.
+                vol = 50;
+                WorkKey = Registry.CurrentUser.OpenSubKey(@"Software\AnimaScreenSaver",true);
+                WorkKey.SetValue("Volume", 50, RegistryValueKind.DWord);
             }
-            return videos;
+            return vol;
+        }
+
+        /// <summary>
+        /// Считывает из реестра значение отвечающее за использование клавиш как регуляторы громкости.
+        /// </summary>
+        /// <returns>Возвращает bool</returns>
+        private bool InitAllow()
+        {
+            bool allow = false;
+            RegistryKey WorkKey = Registry.CurrentUser.OpenSubKey(@"Software\AnimaScreenSaver");
+            int vol = 0;
+            vol = Convert.ToInt32(WorkKey.GetValue("AllowKeys"));
+            if (vol > 1 || vol < 0)
+            {
+                vol = 0;
+                WorkKey = Registry.CurrentUser.OpenSubKey(@"Software\AnimaScreenSaver", true);
+                WorkKey.SetValue("AllowKeys", 0, RegistryValueKind.DWord);
+            }
+            if (vol == 1) allow = true;
+            if (vol == 0) allow = false;
+            return allow;
+        }
+
+        /// <summary>
+        /// Проверяет целостность данных в реестре. В случае отсутствия параметров записывает их все по дефолту.
+        /// </summary>
+        private void CheckReg()
+        {
+            string[] keys = new string[] { };
+            RegistryKey WorkKey = Registry.CurrentUser;
+            WorkKey = WorkKey.OpenSubKey(@"Software", true);
+            keys = WorkKey.GetSubKeyNames();
+            if (!keys.Contains("AnimaScreenSaver"))
+            {
+                CreateReg();
+                return;
+            }
+            WorkKey = WorkKey.OpenSubKey("AnimaScreenSaver");
+            keys = WorkKey.GetValueNames();
+            if ((!keys.Contains("Play"))||(!keys.Contains("Volume"))||(!keys.Contains("AllowKeys")))
+            {
+                CreateReg();
+                return;
+            }
+            return;
+        }
+
+        /// <summary>
+        /// Создает раздел и значения в реестре по умолчанию.
+        /// </summary>
+        private void CreateReg()
+        {
+            RegistryKey WorkKey = Registry.CurrentUser;
+            WorkKey = WorkKey.OpenSubKey(@"Software", true);
+            WorkKey.CreateSubKey("AnimaScreenSaver");
+            WorkKey = WorkKey.OpenSubKey("AnimaScreenSaver", true);
+            WorkKey.SetValue("Play", "", RegistryValueKind.String);
+            WorkKey.SetValue("Volume", 50, RegistryValueKind.DWord);
+            WorkKey.SetValue("AllowKeys", 0, RegistryValueKind.DWord);
+        }
+
+        /// <summary>
+        /// Добавляет список видео в ListBox
+        /// </summary>
+        private void AddVideos()
+        {
+            for(int i = 0; i<videos.Count;i++)
+            {
+                VideoList.Items.Add(videos[i]);
+            }
         }
     }
 } 
