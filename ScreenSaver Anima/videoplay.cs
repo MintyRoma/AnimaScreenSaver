76f6da80b384f35c9c6045c384c4ffa88bae0082
Yan Khachko <luck207869@gmail.com>
2019-06-24 21:37:26 +0300

    test videoplayer v2

--- ScreenSaver Anima/videoplay.cs
+++ ScreenSaver Anima/videoplay.cs
@@ -7,11 +7,15 @@ using WMPLib;
 using Microsoft.DirectX;
 using System.Windows.Forms;
 using System.Drawing;
-using Microsoft.DirectX;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+
 namespace ScreenSaver_Anima
 {
     class videoplay
     {
+        List<string> videos = new List<string>();
         public videoplay()
         {
             Start();
@@ -20,13 +24,65 @@ namespace ScreenSaver_Anima
         private void Start()
         {
             String path = @"C:\Users\yan\Videos\savers\terminal.mp4";
-            Form form1 = new Form();
-            form1.Show();
-            form1.WindowState = FormWindowState.Maximized;
-            Video movie = new Video(path);
-            movie.Owner = form1;
-            movie.Size = new Size(1920, 1080);
-            movie.Play();
+            Tools.GetData();
+            videos = Tools.Videos;
+            bool screen1 = true;
+
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                Form frm = new Form();
+                Video video = null;
+
+                if (screen1)
+                {
+                    video = null;
+                    video = new Video(path);
+                }
+
+
+                try
+                {
+                    video = null;
+                    video = new Video(path);
+                }
+                catch (Exception ex)
+                {
+                    video = null;
+                }
+                finally
+                {
+                    if (video != null)
+                    {
+
+                        video.Owner = frm;
+                        frm.Size = screen.Bounds.Size;
+                        frm.StartPosition = FormStartPosition.Manual;
+                        frm.Location = new Point(screen.Bounds.X, screen.Bounds.Y);
+                        frm.BackColor = System.Drawing.Color.Black;
+                        frm.FormBorderStyle = FormBorderStyle.None;
+                        frm.Size = screen.Bounds.Size;
+                        video.Size = frm.Size;
+
+                        if (screen1)
+                        {
+                            video.Audio.Volume = (Tools.Volume * 100) - 10000;
+                            screen1 = false;
+                        }
+                        else
+                        {
+                            video.Audio.Volume = -10000;
+
+                        }
+                        video.Play();
+                        frm.Show();
+                    }
+                }
+            }
+
+
+            //video.Owner = form1;
+            //video.Size = new Size(1920, 1080);
+            //video.Play();
         }
     }
 }
